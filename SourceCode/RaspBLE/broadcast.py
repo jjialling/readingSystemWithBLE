ce7350286742e8cbbc254bf181c1fad544c221ec
jjialling <dorothy1290@gmail.com>
2024-05-12 00:55:20 +0800

    RaspBLE sourcecode

---
+++ SourceCode/RaspBLE/broadcast.py
@@ -0,0 +1,295 @@
+import logging
+
+import dbus
+import dbus.exceptions
+import dbus.mainloop.glib
+import dbus.service
+
+from ble import (
+    Advertisement,
+    Characteristic,
+    Service,
+    Application,
+    find_adapter,
+    Descriptor,
+    Agent,
+)
+
+import struct
+import requests
+import array
+from enum import Enum
+
+import sys
+
+MainLoop = None
+try:
+    from gi.repository import GLib
+
+    MainLoop = GLib.MainLoop
+except ImportError:
+    import gobject as GObject
+
+    MainLoop = GObject.MainLoop
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+logHandler = logging.StreamHandler()
+filelogHandler = logging.FileHandler("logs.log")
+formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+logHandler.setFormatter(formatter)
+filelogHandler.setFormatter(formatter)
+logger.addHandler(filelogHandler)
+logger.addHandler(logHandler)
+
+
+VivaldiBaseUrl = "XXXXXXXXXXXX"
+
+mainloop = None
+
+BLUEZ_SERVICE_NAME = "org.bluez"
+GATT_MANAGER_IFACE = "org.bluez.GattManager1"
+LE_ADVERTISEMENT_IFACE = "org.bluez.LEAdvertisement1"
+LE_ADVERTISING_MANAGER_IFACE = "org.bluez.LEAdvertisingManager1"
+GATT_CHRC_IFACE = "org.bluez.GattCharacteristic1"
+
+class InvalidArgsException(dbus.exceptions.DBusException):
+    _dbus_error_name = "org.freedesktop.DBus.Error.InvalidArgs"
+
+
+class NotSupportedException(dbus.exceptions.DBusException):
+    _dbus_error_name = "org.bluez.Error.NotSupported"
+
+
+class NotPermittedException(dbus.exceptions.DBusException):
+    _dbus_error_name = "org.bluez.Error.NotPermitted"
+
+
+class InvalidValueLengthException(dbus.exceptions.DBusException):
+    _dbus_error_name = "org.bluez.Error.InvalidValueLength"
+
+
+class FailedException(dbus.exceptions.DBusException):
+    _dbus_error_name = "org.bluez.Error.Failed"
+
+
+def register_app_cb():
+    logger.info("GATT application registered")
+
+
+def register_app_error_cb(error):
+    logger.critical("Failed to register application: " + str(error))
+    mainloop.quit()
+
+class BLEService(Service):
+    
+    F_UUID = '45676d89-d598-4874-8e86-7d042ee07ba7'
+
+    def __init__(self, bus, index):
+        Service.__init__(self, bus, index, self.F_UUID, True)
+        self.add_characteristic(MessageOneChrc(bus, 0, self))
+        self.add_characteristic(MessageTwoChrc(bus, 1, self))
+        self.add_characteristic(MessageThrChrc(bus, 2, self))
+        self.energy_expended = 0
+
+
+class MessageOneChrc(Characteristic):
+    MS_UUID = '4116f8d2-9f66-4f58-a35d-fc7440e7c14e'
+
+    def __init__(self, bus, index, service):
+        Characteristic.__init__(
+                self, bus, index,
+                self.MS_UUID,
+                ['notify','write','read'],
+                service)
+        self.notifying = False
+        self.value = [0x01]
+
+    def Send_value(self):
+        if not self.notifying:
+            return
+
+        self.PropertiesChanged(GATT_CHRC_IFACE, { 'Value': self.value }, [])
+
+        return self.notifying
+    
+
+    def StartNotify(self):
+        if self.notifying:
+            return
+        self.notifying = True
+
+    def StopNotify(self):
+        if not self.notifying:
+            return
+        self.notifying = False
+    
+    def WriteValue(self, value, options):
+        print('Characteristic Write: ' + repr(value))
+        self.value = value
+      
+    def ReadValue(self, options):
+        # Return 'Chest' as the sensor location.
+        print('Characteristic Read: ' + repr(self.value))
+        return self.value
+
+
+class MessageTwoChrc(Characteristic):
+    BODY_UUID = '322e774f-c903-49c4-bd7b-48a4003a967f'
+    def __init__(self, bus, index, service):
+        Characteristic.__init__(self, bus, index, self.BODY_UUID,
+                                ['notify'], service)
+        self.notifying = False
+        GLib.io_add_watch(sys.stdin, GLib.IO_IN, self.on_console_input)
+
+    def on_console_input(self, fd, condition):
+        s = fd.readline()
+       
+        if s.isspace():
+            pass
+        else:
+            self.send_tx(s)
+        return True
+
+    def send_tx(self, s):
+        if not self.notifying:
+            return
+        value = []
+        for c in s:
+            value.append(dbus.Byte(c.encode()))
+        self.PropertiesChanged(GATT_CHRC_IFACE, {'Value': value}, [])
+
+    def StartNotify(self):
+        if self.notifying:
+            return
+        self.notifying = True
+
+    def StopNotify(self):
+        if not self.notifying:
+            return
+        self.notifying = False
+    
+                
+
+class MessageThrChrc(Characteristic):
+    UUID = '9c7dbce8-de5f-4458-89dd-74f04f4e5842'
+    def __init__(self, bus, index, service):
+        Characteristic.__init__(self, bus, index, self.UUID,
+                                ['write'], service)
+
+    def WriteValue(self, value, options):
+       
+        print (''.join('{:02x}'.format(x) for x in value))
+        
+     
+class CharacteristicUserDescriptionDescriptor(Descriptor):
+    """
+    Writable CUD descriptor.
+    """
+
+    CUD_UUID = "2901"
+
+    def __init__(
+        self, bus, index, characteristic,
+    ):
+
+        self.value = array.array("B", characteristic.description)
+        self.value = self.value.tolist()
+        Descriptor.__init__(self, bus, index, self.CUD_UUID, ["read"], characteristic)
+
+    def ReadValue(self, options):
+        return self.value
+
+    def WriteValue(self, value, options):
+        if not self.writable:
+            raise NotPermittedException()
+        self.value = value
+
+
+class VivaldiAdvertisement(Advertisement):
+    def __init__(self, bus, index):
+        Advertisement.__init__(self, bus, index, "peripheral")
+        self.add_manufacturer_data(
+            0xFFFF, [0x70, 0x71],
+        )
+        self.add_service_uuid(BLEService.F_UUID)
+
+        self.add_local_name("raspberrypi")
+        self.include_tx_power = True
+
+
+def register_ad_cb():
+    logger.info("Advertisement registered")
+
+
+def register_ad_error_cb(error):
+    logger.critical("Failed to register advertisement: " + str(error))
+    mainloop.quit()
+
+
+AGENT_PATH = "/com/punchthrough/agent"
+
+
+def main():
+    global mainloop
+
+    dbus.mainloop.glib.DBusGMainLoop(set_as_default=True)
+
+    # get the system bus
+    bus = dbus.SystemBus()
+    # get the ble controller
+    adapter = find_adapter(bus)
+
+    if not adapter:
+        logger.critical("GattManager1 interface not found")
+        return
+
+    adapter_obj = bus.get_object(BLUEZ_SERVICE_NAME, adapter)
+
+    adapter_props = dbus.Interface(adapter_obj, "org.freedesktop.DBus.Properties")
+
+    # powered property on the controller to on
+    adapter_props.Set("org.bluez.Adapter1", "Powered", dbus.Boolean(1))
+
+    # Get manager objs
+    service_manager = dbus.Interface(adapter_obj, GATT_MANAGER_IFACE)
+    ad_manager = dbus.Interface(adapter_obj, LE_ADVERTISING_MANAGER_IFACE)
+
+    advertisement = VivaldiAdvertisement(bus, 0)
+    obj = bus.get_object(BLUEZ_SERVICE_NAME, "/org/bluez")
+
+    agent = Agent(bus, AGENT_PATH)
+
+    app = Application(bus)
+    app.add_service(BLEService(bus, 2))
+
+    mainloop = MainLoop()
+
+    agent_manager = dbus.Interface(obj, "org.bluez.AgentManager1")
+    agent_manager.RegisterAgent(AGENT_PATH, "NoInputNoOutput")
+
+    ad_manager.RegisterAdvertisement(
+        advertisement.get_path(),
+        {},
+        reply_handler=register_ad_cb,
+        error_handler=register_ad_error_cb,
+    )
+
+    logger.info("Registering GATT application...")
+
+    service_manager.RegisterApplication(
+        app.get_path(),
+        {},
+        reply_handler=register_app_cb,
+        error_handler=[register_app_error_cb],
+    )
+
+    agent_manager.RequestDefaultAgent(AGENT_PATH)
+
+    mainloop.run()
+    # ad_manager.UnregisterAdvertisement(advertisement)
+    # dbus.service.Object.remove_from_connection(advertisement)
+
+
+if __name__ == "__main__":
+    main()
